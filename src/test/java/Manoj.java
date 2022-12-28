3272624f52f57e9f598d1e0c050f465b9918233f
Your Name <baranikumarbk96@gmail.com>
2022-12-28 20:14:27 +0530

    filed added to manoj repo

---
+++ src/test/java/Manoj.java
@@ -0,0 +1,34 @@
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Manoj {
+	public static void main(String[] args) {
+		
+		
+	//	System.setProperty("webdriver.chrome.driver","C:\\Users\\sabar\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+		WebDriverManager.chromedriver().setup();
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.flipkart.com/");
+		
+		driver.findElement(By.xpath("/html/body/div[2]/div/div/button")).click();
+		
+		List<WebElement> findElements = driver.findElements(By.xpath("//div[@class='eFQ30H']"));
+		for (int i = 0; i < findElements.size(); i++) {
+			WebElement webElement = findElements.get(i);
+			String text = webElement.getText();
+			System.out.println(text);
+			
+		}
+		
+	}
+
+}
